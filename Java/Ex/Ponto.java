d0c3a9ca8521ba9b6cc67a536f9aea603f7a0b97
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-03-11 11:58:43 -0300

    Update Ponto.java

--- Java/Ex/Ponto.java
+++ Java/Ex/Ponto.java
@@ -2,14 +2,10 @@ public class Ponto{
 	public static void main(String[] args){
 		Ponto2D ponto1 = new Ponto2D(3, 5);
 		Ponto2D ponto2 = new Ponto2D();
-		
-		ponto2.x = 0;
-		ponto2.y = 0;
-		
-		
+			
 		ponto1.mostrarxy();
 		ponto2.mostrarxy();
 		
-		system.out.println(geti());
+		System.out.println(Ponto2D.geti());
 	}
 }
