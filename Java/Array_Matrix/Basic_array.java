e6efe818c71681d6c1723ef0fcb222ce6db5cbde
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-03-13 10:42:13 -0300

    Create Basic_array.java

---
+++ Java/Array_Matrix/Basic_array.java
@@ -0,0 +1,16 @@
+public class Vetormain{
+	public static void main(String[] args){
+		int V1[] = new int [12];
+		int V2[ ] = { 32, 10, 50, 2, 20, 15, 12, 28, 15, 45};
+		
+		for(int i = 0; i < V1.length; i++){
+			System.out.print("V1 = \t"+ V1[i]);
+			System.out.println("");
+		}
+		
+		for(int i = 0; i < V2.length; i++){
+			System.out.print("V2 = \t"+ V2[i]);
+			System.out.println("");
+		}
+	}
+}
