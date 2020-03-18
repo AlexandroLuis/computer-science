f65924a4bc820d71c43a2de6e77dfc268f62e748
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-03-18 18:04:41 -0300

    Update varargs.java

--- Java/Array_Matrix/varargs.java
+++ Java/Array_Matrix/varargs.java
@@ -1,3 +1,30 @@
-public soma{
-
+public class Soma{		
+	private int a;
+	private int b;
+	
+	public Soma(int a, int b){
+		this.a = soma;
+		this.b = soma;
+	}
+	
+	public Soma2(){
+		this.a = soma2;
+		this.b = soma2;
+	}
+	
+	public int soma(int ... Soma){
+		System.out.println(a+b);
+	}
+    
+    public int soma2(int ... Soma2){
+		System.out.println(a+b);
+	}
+    
+    public static void main(String []args){
+	    Soma soma = new Soma(3, 4);
+        Soma soma2 = new Soma2();
+        soma2.a = 1;
+        soma2.b = 2;
+	}
+  
 }
