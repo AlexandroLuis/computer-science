86172d151eb7430d0699699676facdf867568401
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-04-14 20:52:43 -0300

    Update index.php

--- PHP_Projects/cadastro_exercicios/index.php
+++ PHP_Projects/cadastro_exercicios/index.php
@@ -3,62 +3,6 @@
 		<title></title>
 		<meta charset="utf-8">
 		<style>
-		/* Popup container - can be anything you want */
-			.popup {
-			  position: relative;
-			  display: inline-block;
-			  cursor: pointer;
-			  -webkit-user-select: none;
-			  -moz-user-select: none;
-			  -ms-user-select: none;
-			  user-select: none;
-			}
-
-			/* The actual popup */
-			.popup .popuptext {
-			  visibility: hidden;
-			  width: 160px;
-			  background-color: #555;
-			  color: #fff;
-			  text-align: center;
-			  border-radius: 6px;
-			  padding: 8px 0;
-			  position: absolute;
-			  z-index: 1;
-			  bottom: 125%;
-			  left: 50%;
-			  margin-left: -80px;
-			}
-
-			/* Popup arrow */
-			.popup .popuptext::after {
-			  content: "";
-			  position: absolute;
-			  top: 100%;
-			  left: 50%;
-			  margin-left: -5px;
-			  border-width: 5px;
-			  border-style: solid;
-			  border-color: #555 transparent transparent transparent;
-			}
-
-			/* Toggle this class - hide and show the popup */
-			.popup .show {
-			  visibility: visible;
-			  -webkit-animation: fadeIn 1s;
-			  animation: fadeIn 1s;
-			}
-
-			/* Add animation (fade in the popup) */
-			@-webkit-keyframes fadeIn {
-			  from {opacity: 0;} 
-			  to {opacity: 1;}
-			}
-
-			@keyframes fadeIn {
-			  from {opacity: 0;}
-			  to {opacity:1 ;}
-			}
 			body {
 				background-color:white; 
 			}
@@ -124,23 +68,12 @@
 							<td>" .$row['description'] ."</td>
 							<td>" .$row['class'] ."</td>
 							<td>" .$row['level'] ."</td>
-							<td>"?>
-									<div class="popup" onclick="myFunction()">Mostrar Código!
-									<span class="popuptext" id="myPopup">xxxsgsdfgjdfhkghkdfhkghkdafkghdfhagkjx</span>
-								   </div>
-								   <?PHP "</td>";
+							<td>" .$row['source'] ."</td>";
 					$linha++;
 				}
 				mysqli_free_result($result);
 			?>
         </table>
     </div>
-	
-	<script>
-		function myFunction() {
-			var popup = document.getElementById("myPopup");
-			popup.classList.toggle("show");
-		}
-	</script>
 </body>
 </html>
