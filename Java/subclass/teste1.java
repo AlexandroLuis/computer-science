dfdfca48a7ab3f34e98eec80f9b7918ccbf460ed
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-05-14 14:11:50 -0300

    Create teste1.java

---
+++ Java/subclass/teste1.java
@@ -0,0 +1,28 @@
+public class ContaCorrente {
+	private double montante;
+	
+	public Produto(double montante) {
+		this.montante = montante;
+	}
+	public int Sacar(double montante){
+		return Saldo - (Saldo * 0,05);
+	{
+	@Override
+	public String getsaldo() {
+		return saldo;
+	}
+}
+public class Cliente extends ContaCorrente{
+	public int SacarPrivilegio(double montante){
+			return Saldo - (Saldo * 0,01);
+		{
+
+}
+public class Conta {
+	public static void main(String[] args) {
+		Usuario1 = new Saldo(120.05);
+
+		Usuario1.Sacarprivilegio(montante);
+		Usuario1.getsaldo(Usuario);
+	}
+}
