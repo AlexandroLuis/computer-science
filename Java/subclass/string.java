bd41d7d6fa232d84f1c457f32d93df9690754cc0
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-05-15 18:09:45 -0300

    Create string.java

---
+++ Java/subclass/string.java
@@ -0,0 +1,23 @@
+public static void Palavra(){
+	private palavra = new String[];
+	private int soma = 0;
+	
+	if(frase.length < 8){
+		for (int i = 0; i < frase.length; i++){
+			if(i == 1 || i == 2 || i == ((frase.length) -1) || i == ((frase.length) -2))
+				palavra[i] = frase.length - (frase.length - 1);
+		}
+		for (int i = 0; i < 4; i++)
+			System.out.println("Nova palavras = " + palavra[i]);
+	}
+	else
+		System.out.println("erro");
+	}
+}
+
+public class LojaApp {
+	public static void main(String[] args){
+		Frase frase = new frase("nova frase");
+		frase.Palavra();
+	}
+}
