415aa0d8b692c9f85eaf9948281fe57074d0bd49
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-06-16 20:55:24 -0300

    Update queue.c

--- C_Projects/queue/queue.c
+++ C_Projects/queue/queue.c
@@ -10,24 +10,24 @@ int main()
     inicializa_Fila(&P1);
     inicializa_Fila(&P2);
     inicializa_Fila(&P3);
-
+    
     for(i = 10; i < 30; i++)
         insere_Fila(&P1, i);
 
-    printf("Valores da Pilha 1:\n");
+    printf("Valores da Fila 1:\n");
     exibe_Fila(&P1);
     printf("\n\n\n");
 
     comparar(&P1, &P2, &P3);
 
-    printf("Valores da Pilha par:\n");
-    exibe_Fila(&P2);
+   printf("Valores da Fila par:\n");
+	exibe_Fila(&P2);
     printf("\n\n");
 
-    printf("Valores da Pilha impar:\n");
+    printf("Valores da Fila impar:\n");
     exibe_Fila(&P3);
     printf("\n\n\n");
 
-    printf("Valores da nova Pilha 1:\n");
+    printf("Valores da nova Fila 1:\n");
     exibe_Fila(&P1);
 }
