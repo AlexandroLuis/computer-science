ecc84c58dd122dd3892b5fc9252aca794af5047c
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-07-29 23:44:11 -0300

    Create Corda.java

---
+++ Java/Interface/Example/Corda.java
@@ -0,0 +1,11 @@
+public class Sopro implements Instrumento{
+	public void tocar(){
+	   System.out.println("Tocando Flauta...");
+	}
+	public void nome(){
+	  System.out.println("Flauta é o nome do instrumento!");	
+	}
+	public void afinar(){
+    	System.out.println("Verificando a afinação da Flauta.");
+	}
+}
