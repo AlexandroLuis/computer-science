b44416740d75bf9cac23cf5540ecd17cc553eb19
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-07-29 23:44:56 -0300

    Create Main.java

---
+++ Java/Interface/Example/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main (String[] args) {
+	Instrumento[] orquestra = new Instrumento[5];
+	int i = 0;
+	
+    orquestra[i++] = new Sopro();
+    orquestra[i++] = new Corda();
+    orquestra[i++] = new Percussao();
+	orquestra[i++] = new SoproMadeira();
+	orquestra[i]   = new SoproMetal();
+	
+	for(i = 0; i < 5; i++){
+	    System.out.println(orquestra[i].tocar()+" "+orquestra[i].nome()+" "+orquestra[i].afinar());
+	}
+  }
+}
