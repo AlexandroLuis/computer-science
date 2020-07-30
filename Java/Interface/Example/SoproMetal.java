51f11d21ea6700eab362fc97de0565cba4fd5728
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-07-29 23:44:42 -0300

    Create SoproMetal.java

---
+++ Java/Interface/Example/SoproMetal.java
@@ -0,0 +1,11 @@
+public class SoproMetal extends Sopro{
+	public void tocar(){
+	   System.out.println("Tocando Flauta de metal...");
+	}
+	public void nome(){
+	  System.out.println("Flauta de metal é o nome do instrumento!");	
+	}
+	public void afinar(){
+    	System.out.println("Verificando a afinação da Flauta que é de metal.");
+	}
+}
