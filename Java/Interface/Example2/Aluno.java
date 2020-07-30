a66b5512c3ef8ae20f07b1b5590d2dffbbe2536a
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-07-30 15:41:25 -0300

    Create Aluno.java

---
+++ Java/Interface/Example2/Aluno.java
@@ -0,0 +1,11 @@
+public class Aluno implements Falante{
+	public void falar(){
+		System.out.println("Olá!");
+	}
+	public void seuidioma(){
+		System.out.println("Português...");
+	}
+	public void identificarse(){
+		System.out.println("Meu nome é");
+	}
+}
