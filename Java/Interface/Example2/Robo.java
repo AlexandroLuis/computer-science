92f5209099ed55734d93215013b673a480e4c673
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-07-30 15:41:42 -0300

    Create Robo.java

---
+++ Java/Interface/Example2/Robo.java
@@ -0,0 +1,11 @@
+public class Robo implements Falante{
+	public void falar(){
+		System.out.println("Dominar a terra!");
+	}
+	public void seuidioma(){
+		System.ou.println("Navajo");
+	}
+	public void reboot(){
+		System.out.println("Reiniciando...");
+	}
+}
