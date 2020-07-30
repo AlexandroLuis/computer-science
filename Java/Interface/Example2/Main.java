a730daff82601dae77cd28a48fcbe0bb9805310c
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-07-30 15:40:41 -0300

    Update Main.java

--- Java/Interface/Example2/Main.java
+++ Java/Interface/Example2/Main.java
@@ -1,1 +1,15 @@
-varargs
+public class Main {
+	public static void main(String[] args) {
+		Falante f1,f2;
+		
+		f1 = new Aluno();
+		f2 = new Robo();
+		
+		Entrevistador e = new Entrevistador();
+		e.entrevista(f1);
+		for(int i = 0; i < 27; i++)
+		    System.out.printf("-");
+		System.out.println("-");
+		e.entrevista(f2);
+	}
+}
