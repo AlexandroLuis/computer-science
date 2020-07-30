02e05337b08e8fb8d59df4411c9fbdd265a25522
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-07-29 23:44:28 -0300

    Create SoproMadeira.java

---
+++ Java/Interface/Example/SoproMadeira.java
@@ -0,0 +1,11 @@
+public class SoproMadeira extends Sopro{
+	public void tocar(){
+	   System.out.println("Tocando Flauta de madeira");
+	}
+	public void nome(){
+	  System.out.println("Flauta de madeira é o nome do instrumento!");	
+	}
+	public void afinar(){
+    	System.out.println("Verificando a afinação da Flauta que é de madeira.");
+	}
+}
