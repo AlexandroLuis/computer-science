19e53c2ed7a08b99ec4a20a9e76e1e693b304fe9
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-07-29 23:43:57 -0300

    Create Percussao.java

---
+++ Java/Interface/Example/Percussao.java
@@ -0,0 +1,11 @@
+public class Percussao implements Instrumento{
+	public void tocar(){
+	   System.out.println("Tocando Pandeiro...");
+	}
+	public void nome(){
+	  System.out.println("Pandeiro!");	
+	}
+	public void afinar(){
+    	System.out.println("Afinando o Pandeiro");
+	}
+}
