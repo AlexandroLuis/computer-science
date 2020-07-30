f7fb68974df2dcb0dc53ddf9f2eab48f178ffd9c
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-07-30 15:44:21 -0300

    Update Allinone.java

--- Java/Interface/Example/Allinone.java
+++ Java/Interface/Example/Allinone.java
@@ -65,20 +65,20 @@ public class SoproMetal extends Sopro{
 }
 
 public class Main {
-  public static void main (String[] args) {
+    public static void main (String[] args) {
 	Instrumento[] orquestra = new Instrumento[5];
 	int i = 0;
 	
-    orquestra[i++] = new Sopro();
-    orquestra[i++] = new Corda();
-    orquestra[i++] = new Percussao();
-	orquestra[i++] = new SoproMadeira();
+    	orquestra[i++] = new Sopro();
+        orquestra[i++] = new Corda();
+   	orquestra[i++] = new Percussao();
+        orquestra[i++] = new SoproMadeira();
 	orquestra[i]   = new SoproMetal();
 	
 	for(i = 0; i < 5; i++){
 	    orquestra[i].tocar();
-		orquestra[i].nome();
-		orquestra[i].afinar());
+	    orquestra[i].nome();
+	    orquestra[i].afinar());
 	}
-  }
+    }
 }
