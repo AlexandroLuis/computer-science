8fe2e1cf7c8a6a6197d58b422e67e89d77ca97fe
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-08-03 21:59:18 -0300

    Update and rename testte.c to testte.java

---
+++ C/Queue/testte.java
@@ -0,0 +1,306 @@
+Lista de Aviões na Fila!
+Aviões em espera :
+        |Número do Avião: 600, Tipo do Voo: P, Prioridade: 1|
+        |Número do Avião: 641, Tipo do Voo: P, Prioridade: 1|
+        |Número do Avião: 522, Tipo do Voo: P, Prioridade: 1|
+        |Número do Avião: 338, Tipo do Voo: P, Prioridade: 1|
+        |Número do Avião: 627, Tipo do Voo: P, Prioridade: 1|
+        |Número do Avião: 344, Tipo do Voo: P, Prioridade: 2|
+        |Número do Avião: 940, Tipo do Voo: P, Prioridade: 2|
+        |Número do Avião: 604, Tipo do Voo: P, Prioridade: 2|
+        |Número do Avião: 437, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 253, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 464, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 467, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 547, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 516, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 292, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 491, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 181, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 664, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 278, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 410, Tipo do Voo: D, Prioridade: 3|
+
+----------------------------------------------------------------------
+Horário 17:11
+Aviões em espera :
+        |Número do Avião: 600, Tipo do Voo: P, Prioridade: 1|
+        |Número do Avião: 641, Tipo do Voo: P, Prioridade: 1|
+        |Número do Avião: 522, Tipo do Voo: P, Prioridade: 1|
+
+Aviões utilizando a pista no momento:
+        |Avião 600 pousando na pista 0|
+        |Avião 641 pousando na pista 1|
+----------------------------------------------------------------------
+Horário 17:12
+Aviões em espera :
+        |Número do Avião: 522, Tipo do Voo: P, Prioridade: 1|
+        |Número do Avião: 338, Tipo do Voo: P, Prioridade: 1|
+        |Número do Avião: 627, Tipo do Voo: P, Prioridade: 1|
+        |Número do Avião: 344, Tipo do Voo: P, Prioridade: 2|
+
+Existem Aviões utilizando as pistas no momento : 17:12
+
+----------------------------------------------------------------------
+Horário 17:13
+Aviões em espera :
+        |Número do Avião: 522, Tipo do Voo: P, Prioridade: 1|
+        |Número do Avião: 338, Tipo do Voo: P, Prioridade: 1|
+        |Número do Avião: 627, Tipo do Voo: P, Prioridade: 1|
+        |Número do Avião: 344, Tipo do Voo: P, Prioridade: 2|
+        |Número do Avião: 940, Tipo do Voo: P, Prioridade: 2|
+        |Número do Avião: 604, Tipo do Voo: P, Prioridade: 2|
+        |Número do Avião: 437, Tipo do Voo: D, Prioridade: 3|
+
+Aviões utilizando a pista no momento:
+        |Avião 522 pousando na pista 0|
+        |Avião 338 pousando na pista 1|
+----------------------------------------------------------------------
+Horário 17:14
+Aviões em espera :
+        |Número do Avião: 627, Tipo do Voo: P, Prioridade: 1|
+        |Número do Avião: 344, Tipo do Voo: P, Prioridade: 2|
+        |Número do Avião: 940, Tipo do Voo: P, Prioridade: 2|
+        |Número do Avião: 604, Tipo do Voo: P, Prioridade: 2|
+        |Número do Avião: 437, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 253, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 464, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 467, Tipo do Voo: D, Prioridade: 3|
+
+Existem Aviões utilizando as pistas no momento : 17:14
+
+----------------------------------------------------------------------
+Horário 17:15
+Aviões em espera :
+        |Número do Avião: 627, Tipo do Voo: P, Prioridade: 1|
+        |Número do Avião: 344, Tipo do Voo: P, Prioridade: 2|
+        |Número do Avião: 940, Tipo do Voo: P, Prioridade: 2|
+        |Número do Avião: 604, Tipo do Voo: P, Prioridade: 2|
+        |Número do Avião: 437, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 253, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 464, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 467, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 547, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 516, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 292, Tipo do Voo: D, Prioridade: 3|
+
+Aviões utilizando a pista no momento:
+        |Avião 627 pousando na pista 0|
+        |Avião 344 pousando na pista 1|
+----------------------------------------------------------------------
+Horário 17:16
+Aviões em espera :
+        |Número do Avião: 940, Tipo do Voo: P, Prioridade: 2|
+        |Número do Avião: 604, Tipo do Voo: P, Prioridade: 2|
+        |Número do Avião: 437, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 253, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 464, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 467, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 547, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 516, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 292, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 491, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 181, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 664, Tipo do Voo: D, Prioridade: 3|
+
+Existem Aviões utilizando as pistas no momento : 17:16
+
+----------------------------------------------------------------------
+Horário 17:17
+Aviões em espera :
+        |Número do Avião: 940, Tipo do Voo: P, Prioridade: 2|
+        |Número do Avião: 604, Tipo do Voo: P, Prioridade: 2|
+        |Número do Avião: 437, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 253, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 464, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 467, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 547, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 516, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 292, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 491, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 181, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 664, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 278, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 41, Tipo do Voo: D, Prioridade: 3|
+
+Aviões utilizando a pista no momento:
+        |Avião 940 pousando na pista 0|
+        |Avião 604 pousando na pista 1|
+----------------------------------------------------------------------
+Horário 17:18
+Aviões em espera :
+        |Número do Avião: 437, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 253, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 464, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 467, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 547, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 516, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 292, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 491, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 181, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 664, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 278, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 410, Tipo do Voo: D, Prioridade: 3|
+
+Existem Aviões utilizando as pistas no momento : 17:18
+
+----------------------------------------------------------------------
+Horário 17:19
+Aviões em espera :
+        |Número do Avião: 437, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 253, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 464, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 467, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 547, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 516, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 292, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 491, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 181, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 664, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 278, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 410, Tipo do Voo: D, Prioridade: 3|
+
+Aviões utilizando a pista no momento:
+        |Avião 437 decolando na pista 0|
+        |Avião 253 decolando na pista 1|
+----------------------------------------------------------------------
+Horário 17:20
+Aviões em espera :
+        |Número do Avião: 464, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 467, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 547, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 516, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 292, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 491, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 181, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 664, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 278, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 410, Tipo do Voo: D, Prioridade: 3|
+
+Existem Aviões utilizando as pistas no momento : 17:20
+
+----------------------------------------------------------------------
+Horário 17:21
+Aviões em espera :
+        |Número do Avião: 464, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 467, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 547, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 516, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 292, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 491, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 181, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 664, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 278, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 410, Tipo do Voo: D, Prioridade: 3|
+
+Aviões utilizando a pista no momento:
+        |Avião 464 decolando na pista 0|
+        |Avião 467 decolando na pista 1|
+----------------------------------------------------------------------
+Horário 17:22
+Aviões em espera :
+        |Número do Avião: 547, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 516, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 292, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 491, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 181, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 664, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 278, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 410, Tipo do Voo: D, Prioridade: 3|
+
+Existem Aviões utilizando as pistas no momento : 17:22
+
+----------------------------------------------------------------------
+Horário 17:23
+Aviões em espera :
+        |Número do Avião: 547, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 516, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 292, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 491, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 181, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 664, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 278, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 410, Tipo do Voo: D, Prioridade: 3|
+
+Aviões utilizando a pista no momento:
+        |Avião 547 decolando na pista 0|
+        |Avião 516 decolando na pista 1|
+----------------------------------------------------------------------
+Horário 17:24
+Aviões em espera :
+        |Número do Avião: 292, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 491, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 181, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 664, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 278, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 410, Tipo do Voo: D, Prioridade: 3|
+
+Existem Aviões utilizando as pistas no momento : 17:24
+
+----------------------------------------------------------------------
+Horário 17:25
+Aviões em espera :
+        |Número do Avião: 292, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 491, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 181, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 664, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 278, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 410, Tipo do Voo: D, Prioridade: 3|
+
+Aviões utilizando a pista no momento:
+        |Avião 292 decolando na pista 0|
+        |Avião 491 decolando na pista 1|
+----------------------------------------------------------------------
+Horário 17:26
+Aviões em espera :
+        |Número do Avião: 181, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 664, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 278, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 410, Tipo do Voo: D, Prioridade: 3|
+
+Existem Aviões utilizando as pistas no momento : 17:26
+
+----------------------------------------------------------------------
+Horário 17:27
+Aviões em espera :
+        |Número do Avião: 181, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 664, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 278, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 410, Tipo do Voo: D, Prioridade: 3|
+
+Aviões utilizando a pista no momento:
+        |Avião 181 decolando na pista 0|
+        |Avião 664 decolando na pista 1|
+----------------------------------------------------------------------
+Horário 17:28
+Aviões em espera :
+        |Número do Avião: 278, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 410, Tipo do Voo: D, Prioridade: 3|
+
+Existem Aviões utilizando as pistas no momento : 17:28
+
+----------------------------------------------------------------------
+Horário 17:29
+Aviões em espera :
+        |Número do Avião: 278, Tipo do Voo: D, Prioridade: 3|
+        |Número do Avião: 410, Tipo do Voo: D, Prioridade: 3|
+
+Aviões utilizando a pista no momento:
+        |Avião 278 decolando na pista 0|
+        |Avião 410 decolando na pista 1|
+----------------------------------------------------------------------
+Horário 17:30
+Sem Aviões em espera!
+
+
+Existem Aviões utilizando as pistas no momento : 17:30
+
+----------------------------------------------------------------------
+Horário 17:31
+Sem Aviões em espera!
+
+
+----------------------------------------------------------------------
+Horário 17:31
+
+Todos os pousos e decolagens realizados!
