18777e65ae46657edf35072b19f88b7851cc6f82
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-08-12 11:34:54 -0300

    Create Example1.java

---
+++ Java/Exception/Example1.java
@@ -0,0 +1,10 @@
+public class Main{
+  public static void main(String[] args) {
+    try{
+		    int[] numero = new int[] { 2, 4, 6, 8, 10 }; 
+		    for (int i = 0; i < 6; i++) 
+			      System.out.println(vetor[i]); 
+	  }
+	  catch(ArrayIndexOutOfBoundsException e){
+		    System.out.println("Final do vetor!") ;
+  	}
