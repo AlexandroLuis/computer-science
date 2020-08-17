da7c746039f6b9fc8ddc87dd03b91db7fb51a1a6
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-08-17 16:20:00 -0300

    Add files via upload

--- PHP/UriExamples/Admin.php
+++ PHP/UriExamples/Admin.php
@@ -7,94 +7,58 @@
 ?>
 <html>
 	<head>
-		<title></title>
+		<title>Uriexamples</title>
 		<meta charset="utf-8">
+		<link rel="stylesheet" href="Style/style.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-		<link rel="stylesheet" href="style.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-		<style>
-			a:link, a:visited {
-				text-decoration: none;
-			}
-			a:hover {
-				text-decoration: none; 
-				color: none;
-			}
-			.dropbtn:hover, .dropbtn:focus {
-			  background-color: #2980B9;
-			}
-
-			.dropdown {
-			  position: relative;
-			  display: inline-block;
-			}
-
-			.dropdown-content {
-			  display: none;
-			  position: absolute;
-			  background-color: #f1f1f1;
-			  min-width: 100px;
-			  overflow: auto;
-			  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-			  z-index: 1;
-			}
-
-			.dropdown-content a {
-			  color: black;
-			  padding: 10px 13px;
-			  text-decoration: none;
-			  display: block;
-			}
-
-			.dropdown a:hover {background-color: #ddd;}
-
-			.show {display: block;}
-		</style>
 	</head>
 <body>
-	<ul>
+	<ul><!-- Menu Inicial -->
 		<li style="float:left"><a>Bem Vindo, <?php echo $_SESSION['usuario'];?>!</a></li>
 		<li style="float:right"><a href="logout.php">Sair</a></li>
 		<li style="float:right"><a href="Cex.php">Inserir Novo</a></li>	
-	</ul>
+	</ul><!-- Fim Menu Inicial -->
 	
-	<div class="dropdown">
+	<div class="tab-container"><!-- Lista De Problemas -->
         <h2 align ="left">Seleção de Problemas do URI Online Judge!</h2><br>
         <table border="0" style="text-align:left;">
             <tr>
-                <th width="50"  style="text-align:left;">ID</th>
-                <th width="250"  style="text-align:left;">Nome</th>
-				<th width="150"  style="text-align:left;">
+				<th width="10"></th>
+                <th width="75">ID</th>
+                <th width="230">Nome</th>
+				<th width="150">
 					<a onclick="classificacao()" class="dropbtn">Categoria</a>
 						<div id="classificacao" class="dropdown-content">
 							<a href="Admin.php?ordem=class&campo=data">Ordenar por Categoria</a>
 						</div>
 				</th>
-				<th width="75"  style="text-align:left;">
+				<th width="75">
 					<a onclick="dificuldade()" class="dropbtn">Nivel</a>
 						<div id="dificuldade" class="dropdown-content">
 							<a href="Admin.php?ordem=dificuldade1&campo=data">Crescente</a>
 							<a href="Admin.php?ordem=dificuldade2&campo=data">Decrescente</a>
 						</div>
 				</th>
-                <th width="405"  style="text-align:left;">Descrição</th>
-				<th width="100"  style="text-align:left;">
+                <th width="415">Descrição</th>
+				<th width="160">
 					<a onclick="tipo()" class="dropbtn">Classificação</a>
 						<div id="tipo" class="dropdown-content">
 							<a href="Admin.php?ordem=tipo1&campo=data">Ordenar Por tipo</a>
 						</div>
 				</th>			 					
-				<th width="150"  style="text-align:left;">
+				<th width="120" >
 					<a onclick="dificuldadee2pc()" class="dropbtn">Dificuldade</a>
 						<div id="dificuldadee2pc" class="dropdown-content">
 							<a href="Admin.php?ordem=dificuldadee2pc1&campo=data">Crescente</a>
 							<a href="Admin.php?ordem=dificuldadee2pc2&campo=data">Decrescente</a>
 						</div>
 				</th>	
-				<th width="100"  style="text-align:left;">Código</th>
-            </tr>
-			<script> // Função para fazer o  dropdown
+				<th width="100">Código</th>
+            </tr><!-- Fim Lista de Problemas -->
+			
+			<script><!-- Função para fazer o  dropdown -->
 				function classificacao() {
 				  document.getElementById("classificacao").classList.toggle("show");
 				}	
@@ -123,7 +87,7 @@
 					}
 				  }
 				}
-			</script>
+			</script><!-- Fim da Função dropdown -->
 
 			<?php
 				ini_set('display_errors', 0 );
@@ -157,72 +121,15 @@
 				$linha =1;
 				while($row = mysqli_fetch_assoc($result)) {
 					echo "<tr>
-							<td>" .$row['id'] ."</td>
+							<td></td>
+							<td>" ."<a href=editarproblema.php?id=".$row['id'] .">".$row['id'] ."</a></td>
 							<td>" ."<a href=https://www.urionlinejudge.com.br/judge/pt/problems/view/".$row['id'] ."''>".$row['name'] ."</a></td>
 							<td>" .$row['class'] ."</td>
 							<td>" .$row['level'] ."</td>
-							<td>"?>
-								<a href="" data-toggle="modal" data-target="#exampleModalLong">Mostrar Descrição</a>
-									<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-									  <div class="modal-dialog" role="document">
-										<div class="modal-content">
-										  <div class="modal-header">
-											<h5 class="modal-title">
-												<?php
-													echo"Descrição Do exercicio: " .$row['id'] ."";
-												?>											
-											</h5>
-											<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-												<span aria-hidden="true">&times;</span>
-											</button>
-										  </div>
-										  <div class="modal-body">
-											<?php
-												echo"" .$row['description'] ."";
-												echo"\nObservação:\n";
-												echo"" .$row['Observation'] ."";
-												
-												
-											?>
-										  </div>
-										  <div class="modal-footer">
-											<button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
-										  </div>
-										</div>
-									  </div>
-									</div>
-								<?php
-							"</td>
+							<td>" .$row['description'] ."</td>
 							<td>" .$row['type'] ."</td>
 							<td>" .$row['level2pc'] ."</td>							
-							<td>"?>
-								<a href="" data-toggle="modal" data-target="#exampleModalLong">Mostrar Código</a>
-									<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-									  <div class="modal-dialog" role="document">
-										<div class="modal-content">
-										  <div class="modal-header">
-											<h5 class="modal-title">
-												<?php
-													echo"Código do exercício: " .$row['id'] ."";
-												?>											
-											</h5>
-											<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-												<span aria-hidden="true">&times;</span>
-											</button>
-										  </div>
-										  <div class="modal-body">
-											<?php
-												echo"" .$row['source'] ."";
-											?>
-										  </div>
-										  <div class="modal-footer">
-											<button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
-										  </div>
-										</div>
-									  </div>
-									</div>
-								<?php
-							"</td>";
+							<td>" ."<a href=Mostrarcodigo.php?id=".$row['id'] .">Mostrar Código</a></td>";
 					$linha++;
 				}
 				mysqli_free_result($result);
---
+++ PHP/UriExamples/Mostrarcodigo.php
@@ -0,0 +1,47 @@
+<!--  
+	Desenvolvido por https://github.com/Alexandro-845
+	Versão 1.0 - 2020
+-->
+<?php
+	ini_set('display_errors', 0 );
+	error_reporting(0);
+	
+	include('verifica_login.php');
+?>
+<html>
+	<head>
+		<title>Uriexamples</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="Style/style.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	</head>
+<body>
+	<ul><!-- Menu Inicial -->
+		<li style="float:left"><a>Bem Vindo, <?php echo $_SESSION['usuario'];?>!</a></li>
+		<li style="float:right"><a href="logout.php">Sair</a></li>
+		<li style="float:right"><a href="Admin.php">Voltar</a></li>	
+	</ul><!-- Fim Menu Inicial -->
+
+    <h2 align ="left">Código do Exercicio <?php  $id=$_GET['id']; echo"$id"; ?>!</h2><br>
+
+	<!-- Recebe Um id Por Parametro e Mostra o resultado do código -->
+	<table border="0" width="500">
+	<?php
+		$id=$_GET['id'];
+		require('conexao.php');
+		
+		$result = mysqli_query($db, "SELECT * FROM exercicio where id=$id");
+		$linha =1;
+		
+		$row = mysqli_fetch_assoc($result);
+		echo"<tr>
+				<td>" .$row['source'] ."</td>
+			 </tr>";
+
+		mysqli_free_result($result);
+	?>
+	</table>
+</body>
+</html>
