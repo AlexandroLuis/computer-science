8e07335b8bea9247e1172681bf6bd8a02f6f174e
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-08-28 10:34:53 -0300

    Create Pessoa.java

---
+++ Java/SubClass/Example6/Pessoa.java
@@ -0,0 +1,13 @@
+public class Pessoa{
+    public String nome;
+    
+    public Pessoa(String nome){
+        this.nome = nome;
+    }
+	public String getNome(){
+		return nome;
+	}
+	public void setNome(String nome) {
+	  this.nome = nome;
+	}
+}
