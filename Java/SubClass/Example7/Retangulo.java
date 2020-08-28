16f0795b97cda483abfb6626627c640ae7028f51
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-08-28 10:36:29 -0300

    Create Retangulo.java

---
+++ Java/SubClass/Example7/Retangulo.java
@@ -0,0 +1,8 @@
+public class Retangulo extends Figura {
+    public void desenhar() {
+		System.out.println("Desenhando Retangulo");
+	}
+	public void apagar() {
+		System.out.println("Apagando Retangulo");
+	}
+}
