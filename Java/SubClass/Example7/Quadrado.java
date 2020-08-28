bca05ae14d512cfb06f44f2987bd9eca701d4b83
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-08-28 10:36:46 -0300

    Create Quadrado.java

---
+++ Java/SubClass/Example7/Quadrado.java
@@ -0,0 +1,8 @@
+public class Quadrado extends Figura {
+    public void desenhar() {
+		System.out.println("Desenhando Quadrado");
+	}
+	public void apagar() {
+		System.out.println("Apagando Quadrado");
+	}
+}
