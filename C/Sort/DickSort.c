3e9c8fbbd289d9fee6a903448ca7fec3e34fb781
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-09-07 13:48:03 -0300

    Update DickSort.c

--- C/Sort/DickSort.c
+++ C/Sort/DickSort.c
@@ -6,46 +6,50 @@
 #include <stdlib.h>
 
 /* Swap Values case Array[i] > Array[i+1]  */
-void Swap(int *a, int n){ 
-    int i, t, r;
-    for(i = 0; i < n-1; i++) {
-        if(a[i] > a[i+1]){
-            t = a[i];
-            a[i] = a[i+1];
-            a[i+1] = t;
+void Swap(int *V, int s){
+    int i, aux;
+    for(i = 0; i < s-1; i++) {
+        if(*(V+i) > *(V+i+1)){
+            aux = *(V+i);
+            *(V+i) = *(V+i+1);
+            *(V+i+1) = aux;
         }
     }
 }
 
 /* Verify if Array[i] > Array[i+1] and return stop value to Sort function  */
-int Verify(int *a, int n){ 
-    while(--n >= 1){
-        if(a[n] < a[n-1])
+int Verify(int *V, int s){
+    while(--s >= 1){
+        if(*(V+s) < *(V+s-1))
             return 0;
     }
     return 1;
 }
 
-/* Case Verify Still false, Swap values */
-void Sort(int *a, int n){
-    while(Verify(a, n) != 1)
-        Swap(a, n);
+/* Recursive, if Verify Still false, Swap values */
+int Sort(int *V, int s){
+    if(Verify(V, s) != 1){
+        Swap(V, s);
+        Sort(V, s);
+    }
 }
 
 int main(){
-    int i, V[] = {1,2,	3,	18,	90,	70,	25,	33,	38,	57,	82,
-                  50, 38,	65,	40,	79,	18,	92,	92,	50,	50,
-                  31,	28,	25,	31,	19,	71,	86,	97,	81,	62,
-                  1,	5,	57,	52,	68,	49,	29,	9,	20,	5,
-                  77,	17,	39,	52,	89,	56,	49,	65,	89};
+    int i, *V, s = 50;
+
+    V = malloc(s* sizeof(int));
+
+    for(i = 0; i < s; i++){
+        *(V+i) = rand()%100;
+    }
 
     printf("Unordered:\n");
-    for(i = 0; i < 50; i++)
-        printf("%d ", V[i]);
+    for(i = 0; i < s; i++)
+        printf("%d ", *(V+i));
 
-    Sort(V, 50);/* 50 is Array size */
+    Sort(V, s);/* 50 is Array size */
 
     printf("\nOrdered:\n");
-    for(i = 0; i < 50; i++)
-        printf("%d ", V[i]);
+    for(i = 0; i < s; i++)
+        printf("%d ", *(V+i));
 }
