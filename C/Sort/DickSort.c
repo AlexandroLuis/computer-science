4d4df65a5f465f63b3dce0a06d8e15a3e4368dcc
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-09-07 13:06:36 -0300

    Create DickSort.c

---
+++ C/Sort/DickSort.c
@@ -0,0 +1,48 @@
+/*
+    DickSort Developed by Alexandro 2020 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void Swap(int *a, int n){ // SWAP A[I] A[I+1]
+    int i, t, r;
+    for(i = 0; i < n-1; i++) {
+        if(a[i] > a[i+1]){
+            t = a[i];
+            a[i] = a[i+1];
+            a[i+1] = t;
+        }
+    }
+}
+
+int Verify(int *a, int n){// VERIFY IF A[I] > A[I+I]
+    while(--n >= 1){
+        if(a[n] < a[n-1])
+            return 0;
+    }
+    return 1;
+}
+
+void Sort(int *a, int n){
+    while(Verify(a, n) != 1)//SWAP IF VERIFY IS FALSE
+        Swap(a, n);
+}
+
+int main(){
+    int i, V[] = {7,35,	97,	18,	90,	70,	25,	33,	38,	57,	82,
+                  50, 38,	65,	40,	79,	18,	92,	92,	50,	50,
+                  31,	28,	25,	31,	19,	71,	86,	97,	81,	62,
+                  1,	5,	57,	52,	68,	49,	29,	9,	20,	5,
+                  77,	17,	39,	52,	89,	56,	49,	65,	89};
+
+    printf("Unordered:\n");
+    for(i = 0; i < 50; i++)
+        printf("%d ", V[i]);
+
+    Sort(V, 50);// 50 is n parameter
+
+    printf("\nOrdered:\n");
+    for(i = 0; i < 50; i++)
+        printf("%d ", V[i]);
+}
