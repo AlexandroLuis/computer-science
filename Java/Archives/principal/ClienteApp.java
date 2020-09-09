ec3dbd1974b3376ddb41db5a7ea8033578790b17
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-09-09 19:47:14 -0300

    Update ClienteApp.java

--- Java/Archives/principal/ClienteApp.java
+++ Java/Archives/principal/ClienteApp.java
@@ -5,18 +5,18 @@ import entradadados.*;
 
 public class ClienteApp {/* Classe ClienteApp */
 	public static void main(String[] args) {
-		GerenciadorClientesArquivo gerenciador = new GerenciadorClientesArquivo(); /* Declara uma variável gerenciador de arquivos */
-		Cliente[] cliente = new Cliente[10]; /* Declara um vetor de 10 posições para armazenar o nome dos clientes */
-		inicializaClientes(cliente); /* Chama o método para declarar o vetor clientes */
+		GerenciadorClientesArquivo gerenciador = new GerenciadorClientesArquivo(); /* Declara uma variÃ¡vel gerenciador de arquivos */
+		Cliente[] cliente = new Cliente[10]; /* Declara um vetor de 10 posiÃ§Ãµes para armazenar o nome dos clientes */
+		inicializaClientes(cliente); /* Chama o mÃ©todo para declarar o vetor clientes */
 		for(int i = 0; i < 10 ; i++){ /* Salvar o nome dos 10 clientes */
-			System.out.printf("Informe o %d� cliente: ", i+1);
+			System.out.printf("Informe o %d° cliente: ", i+1);
 			cliente[i].setNome(Console.input());
 		}
-		gerenciador.gravarClientes(cliente); /* Chama o Método e Grava o nome dos clientes no arquivo */
+		gerenciador.gravarClientes(cliente); /* Chama o MÃ©todo e Grava o nome dos clientes no arquivo */
 		System.out.println();
-		gerenciador.lerClientes(); /* Chama o Método para mostrar o nome dos clientes  */
+		gerenciador.lerClientes(); /* Chama o MÃ©todo para mostrar o nome dos clientes  */
 	}
-	public static void inicializaClientes(Cliente[] cliente) { /* Método que declara o vetor cliente */
+	public static void inicializaClientes(Cliente[] cliente) { /* MÃ©todo que declara o vetor cliente */
 		for(int i = 0; i < 10; i++) {
 			cliente[i] = new Cliente();
 		}
