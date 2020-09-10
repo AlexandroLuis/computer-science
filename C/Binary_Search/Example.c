778b7c5a5f5b0eb42a93036cc8a1dd072a499bad
Alexandro <52381045+Alexandro-845@users.noreply.github.com>
2020-09-10 12:37:02 -0300

    Update Example.c

--- C/Binary_Search/Example.c
+++ C/Binary_Search/Example.c
@@ -1,48 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int leitura(int *V, int tam){
-    printf("\nDigite os valores : ");
-    for(int i = 0; i < tam; i++)
-        scanf("%d", (V+i));
-}
+void Sort(int *V, int tam){
+    int aux = 0;
 
-int binario(int *V, int tam, int procura){
-    int meio, fim = (tam-1), i = 0, g = 0, aux = 0, Cont = 0;
-
-
-    for(int i = 0; i < tam-1; i++)
-    {
-        for(int j = 0; j < tam-1-i; j++)
-        {
-            if(*(V+j) > *(V+j+1))
-            {
+    for(int i = 0; i < tam-1; i++){
+        for(int j = 0; j < tam-1-i; j++){
+            if(*(V+j) > *(V+j+1)){
                 aux = *(V+j);
                 *(V+j) = *(V+j+1);
                 *(V+j+1) = aux;
-                Cont++;
             }
         }
     }
-    printf("Vetor ordenado: ");
-    for(int i = 0; i < tam; i++)
-        printf("%d\t", *(V+i));
-    printf("\n");
+}
 
-    while(fim >= i)
-    {
+bool BinarySearch(int *V, int tam, int procura){
+    int meio, fim = (tam-1), i = 0, aux = 0;
+    Sort(V, tam);
+
+    while(fim >= i){
         meio = (i+fim)/2;
         if(procura == *(V+meio))
-            return 1;
-        else
-        {
+            return true;
+        else{
             if(procura < *(V+meio))
                 fim = meio-1;
             else
                 i = meio+1;
         }
     }
-    return 0;
+    return false;
 }
 
 
@@ -51,15 +40,16 @@ int main(){
 
     printf("Digite o tamanho do vetor: ");
     scanf("%d", &tam);
-
     V = malloc(tam * sizeof(int));
 
-  
-    printf("Busca binaria Escolhida.\n");
-    printf("digite o valor a ser procurado :  ");
+    printf("\ndigite o valor a ser procurado :  ");
     scanf("%d", &procura);
-    leitura(V, tam);
-    printf("%d",binario(V, tam, procura));
+
+    printf("\nDigite os valores:\n");
+    for(int i = 0; i < tam; i++)
+        scanf("%d", (V+i));
+
+    (BinarySearch(V, tam, procura) == true)?printf("Encontrado!"):printf("Não Encontrado!");
 
     free(V);
     V = NULL;
