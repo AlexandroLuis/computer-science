8b76cb39a4e8aff58cd7d7df02d36fe504e479ac
Alex <52381045+AlexandroLuis@users.noreply.github.com>
2020-12-02 09:55:18 -0300

    Create Main.java

---
+++ Java/OOP-II/CollectionsFramework/HashMap/Example1/Main.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Main {
+  public static void main(String[] args) {
+    HashMap<String, Double> Frutas = new HashMap<>();
+    Frutas.put("Tomate", 20.32);
+    Frutas.put("Mamão", 12.52);
+    Frutas.put("Acerola", 34.12);
+    Frutas.put("Quiabo", 16.82);
+	Frutas.put("Kiwi", 40.40);
+	
+    System.out.println(Frutas); 
+  }
+}
