0b72a5e3f5ede3e0530eec71db304286a3b7aa60
Alex <52381045+AlexandroLuis@users.noreply.github.com>
2021-02-24 17:36:02 -0300

    Update Main.java

--- Java/OOP-II/JavaDB/Example1/Main.java
+++ Java/OOP-II/JavaDB/Example1/Main.java
@@ -1,3 +1,11 @@
+package Pack02748;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Main {
 	public static void main(String[] args) {
 		String url = "jdbc:postgresql://localhost:5432/MCH"; 
