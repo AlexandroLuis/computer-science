5ef578ac653da07bc810202feb98025b7fa17d6d
Alex <52381045+AlexandroLuis@users.noreply.github.com>
2021-09-15 18:08:34 -0300

    Update 8Puzzle.py

--- Python/Problems/8Puzzle.py
+++ Python/Problems/8Puzzle.py
@@ -1,122 +1,121 @@
 from copy import deepcopy
-from colorama import *
 
-DIRECTIONS = {"U": [-1, 0], "D": [1, 0], "L": [0, -1], "R": [0, 1]}
-END = [[1, 2, 3], [8, 0, 4], [7, 6, 5]]
+Direcoes = {"Cima": [-1, 0], "Baixo": [1, 0], "Esquerda": [0, -1], "Direita": [0, 1]}
+Objetivo = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
 
-def print_puzzle(array):
-    for a in range(len(array)):
-        for i in array[a]:
-            if i == 0:
-                print(" ", Back.RED + ' ' + Back.RESET, end=' ')
-            else:
-                print(" ", i, end=' ')
-        print("")
-
-
+# Classe para a Criação dos Nós
 class Node:
-    def __init__(self, current_node, previous_node, g, h, dir):
-        self.current_node = current_node
-        self.previous_node = previous_node
+    def __init__(self, NoAtual, NoAnterior, g, h, dir):
+        self.NoAtual = NoAtual
+        self.NoAnterior = NoAnterior
         self.g = g
         self.h = h
         self.dir = dir
 
     def f(self):
         return self.g + self.h
+        
+# Função para Mostrar os caminhos
+def Saida(No):
+    for i in range(len(No)):
+        for j in No[i]:
+            if j == 0:
+                print("  ", end=' ')
+            else:
+                print(" ", j, end=' ')
+        print("")
 
+def RetornaPosicao(EstadoAtual, Valor):
+    for i in range(len(EstadoAtual)):
+        if Valor in EstadoAtual[i]:
+            return (i, EstadoAtual[i].index(Valor))
 
-def get_pos(current_state, element):
-    for row in range(len(current_state)):
-        if element in current_state[row]:
-            return (row, current_state[row].index(element))
+# Calcula o custo euclediano
+def CustoEuclediano(EstadoAtual):
+    SomaCusto = 0
+    for i in range(len(EstadoAtual)):
+        for j in range(len(EstadoAtual[0])):
+            Posicao = RetornaPosicao(Objetivo, EstadoAtual[i][j])
+            SomaCusto += abs(i - Posicao[0]) + abs(j - Posicao[1])
 
+    return SomaCusto
 
-def euclidianCost(current_state):
-    cost = 0
-    for row in range(len(current_state)):
-        for col in range(len(current_state[0])):
-            pos = get_pos(END, current_state[row][col])
-            cost += abs(row - pos[0]) + abs(col - pos[1])
-    return cost
-
-
-def getAdjNode(node):
+def NoAdjacente(node):
     listNode = []
-    emptyPos = get_pos(node.current_node, 0)
+    PosicaoVazia = RetornaPosicao(node.NoAtual, 0)
 
-    for dir in DIRECTIONS.keys():
-        newPos = (emptyPos[0] + DIRECTIONS[dir][0], emptyPos[1] + DIRECTIONS[dir][1])
-        if 0 <= newPos[0] < len(node.current_node) and 0 <= newPos[1] < len(node.current_node[0]):
-            newState = deepcopy(node.current_node)
-            newState[emptyPos[0]][emptyPos[1]] = node.current_node[newPos[0]][newPos[1]]
-            newState[newPos[0]][newPos[1]] = 0
-            # listNode += [Node(newState, node.current_node, node.g + 1, euclidianCost(newState), dir)]
-            listNode.append(Node(newState, node.current_node, node.g + 1, euclidianCost(newState), dir))
+    for dir in Direcoes.keys():
+        NovaPosicao = (PosicaoVazia[0] + Direcoes[dir][0], PosicaoVazia[1] + Direcoes[dir][1])
+        if 0 <= NovaPosicao[0] < len(node.NoAtual) and 0 <= NovaPosicao[1] < len(node.NoAtual[0]):
+            NovoEstado = deepcopy(node.NoAtual)
+            NovoEstado[PosicaoVazia[0]][PosicaoVazia[1]] = node.NoAtual[NovaPosicao[0]][NovaPosicao[1]]
+            NovoEstado[NovaPosicao[0]][NovaPosicao[1]] = 0
+            listNode.append(Node(NovoEstado, node.NoAtual, node.g + 1, CustoEuclediano(NovoEstado), dir))
 
     return listNode
 
+# Busca pelo melhor caminho
+def RetornoMelhorCaminho(Abertos):
+    TesteInicial = True
 
-def getBestNode(openSet):
-    firstIter = True
+    for no in Abertos.values():
+        if TesteInicial or no.f() < Melhor:
+            TesteInicial = False
+            MelhorNo = no
+            Melhor = MelhorNo.f()
+    return MelhorNo
 
-    for node in openSet.values():
-        if firstIter or node.f() < bestF:
-            firstIter = False
-            bestNode = node
-            bestF = bestNode.f()
-    return bestNode
+# Constroi os caminhos
+def ConstruirCaminho(Fechados):
+    no = Fechados[str(Objetivo)]
+    Desvios = list()
 
-
-def buildPath(closedSet):
-    node = closedSet[str(END)]
-    branch = list()
-
-    while node.dir:
-        branch.append({
-            'dir': node.dir,
-            'node': node.current_node
+    while no.dir:
+        Desvios.append({
+            'dir': no.dir,
+            'node': no.NoAtual
         })
-        node = closedSet[str(node.previous_node)]
-    branch.append({
+        no = Fechados[str(no.NoAnterior)]
+
+    Desvios.append({
         'dir': '',
-        'node': node.current_node
+        'node': no.NoAtual
     })
-    branch.reverse()
 
-    return branch
-
+    Desvios.reverse()
+    return Desvios
 
 def main(puzzle):
-    open_set = {str(puzzle): Node(puzzle, puzzle, 0, euclidianCost(puzzle), "")}
-    closed_set = {}
+    Abertos = {str(puzzle): Node(puzzle, puzzle, 0, CustoEuclediano(puzzle), "")}
+    Fechados = {}
 
+    # Busca
     while True:
-        test_node = getBestNode(open_set)
-        closed_set[str(test_node.current_node)] = test_node
+        NoDeTeste = RetornoMelhorCaminho(Abertos)
+        Fechados[str(NoDeTeste.NoAtual)] = NoDeTeste
 
-        if test_node.current_node == END:
-            return buildPath(closed_set)
+        if NoDeTeste.NoAtual == Objetivo:
+            return ConstruirCaminho(Fechados)
 
-        adj_node = getAdjNode(test_node)
+        adj_node = NoAdjacente(NoDeTeste)
         for node in adj_node:
-            if str(node.current_node) in closed_set.keys() or str(node.current_node) in open_set.keys() and open_set[
-                str(node.current_node)].f() < node.f():
-                continue
-            open_set[str(node.current_node)] = node
+            if str(node.NoAtual) in Fechados.keys() or str(node.NoAtual) in Abertos.keys() and Abertos[
+                str(node.NoAtual)].f() < node.f():
+                    continue
+            Abertos[str(node.NoAtual)] = node
 
-        del open_set[str(test_node.current_node)]
+        del Abertos[str(NoDeTeste.NoAtual)]
 
 if __name__ == '__main__':
     cont = 0
-    br = main([[2, 8, 3], [1, 6, 4], [7, 0, 5]])
+    Entrada = main([[1, 2, 0], [4, 5, 3], [7, 8, 6]])
 
-    print(f'Movimentações: ', len(br) - 1)
-    for b in br:
+    print(f'Movimentações: ', len(Entrada) - 1)
+    for i in Entrada:
         cont += 1
         print()
-        if cont == len(br):
+        if cont == len(Entrada):
             print("Resultado:")
         else:
             print(f'Passo:', cont)
-        print_puzzle(b['node'])
+        Saida(i['node'])
