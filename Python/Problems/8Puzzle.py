d18eedeb8087de138ba681c6bc95254fa033b99c
Alex <52381045+AlexandroLuis@users.noreply.github.com>
2021-09-20 15:21:53 -0300

    Update 8Puzzle.py

--- Python/Problems/8Puzzle.py
+++ Python/Problems/8Puzzle.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 from random import sample
+import sys
 
 Direcoes = {"Cima": [-1, 0], "Baixo": [1, 0], "Esquerda": [0, -1], "Direita": [0, 1]}
 Objetivo = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
@@ -115,14 +116,13 @@ def ConstruirCaminho(Fechados):
 
 
 def BuscaSolucao(Entrada):
-    counter = 2
+    counter = 0
 
     for i in range(2):
         j = i + 1
         for j in range(3):
-            if (Entrada[j][i] > 0 and Entrada[j][i] > 0 and Entrada[j][i] > Entrada[i][j]):
-                counter += 0
-                #print(counter)
+            if (int(Entrada[j][i]) > 0 and int(Entrada[j][i]) > 0 and int(Entrada[j][i]) > int(Entrada[i][j])):
+                counter += 1
 
     return True if counter % 2 == 0 else False
 
@@ -131,43 +131,62 @@ def BuscaSolucao(Entrada):
 
 '''
 def Start(Entrada):
+    if BuscaSolucao(Entrada) == False:
+        return False
     Abertos = {str(Entrada): Node(Entrada, Entrada, 0, DistanciaEuclediana(Entrada), "")}
     Fechados = {}
-    Solucionavel = BuscaSolucao(Entrada)
+    j = 2
 
     '''
         Realiza as buscas
-    
+
     '''
-    if Solucionavel:
-        #A*
+    if j == 1:
+        # A*
         while True:
             NoDeTeste = RetornoMelhorCaminho(Abertos)
             Fechados[str(NoDeTeste.NoAtual)] = NoDeTeste
 
             if NoDeTeste.NoAtual == Objetivo:
-                return ConstruirCaminho(Fechados)
+               print("A*")
+               return ConstruirCaminho(Fechados)
 
             NosAdjacentes = NoAdjacente(NoDeTeste)
             for no in NosAdjacentes:
-                if str(no.NoAtual) in Fechados.keys() or str(no.NoAtual) in Abertos.keys() and Abertos[
-                    str(no.NoAtual)].Soma() < no.Soma():
+                if str(no.NoAtual) in Fechados.keys() or str(no.NoAtual) in Abertos.keys() and Abertos[str(no.NoAtual)].Soma() < no.Soma():
                     continue
                 Abertos[str(no.NoAtual)] = no
 
             del Abertos[str(NoDeTeste.NoAtual)]
 
-        #Dfs
+    elif j == 2:
+        # Dfs
         while True:
             NoDeTeste = RetornoMelhorCaminho(Abertos)
             Fechados[str(NoDeTeste.NoAtual)] = NoDeTeste
 
             if NoDeTeste.NoAtual == Objetivo:
+                print("Dfs")
                 return ConstruirCaminho(Fechados)
 
             NosAdjacentes = NoAdjacente(NoDeTeste)
             for no in NosAdjacentes:
-                
+                if no not in Fechados:
+                    Abertos[str(no.NoAtual)] = no
+            del Abertos[str(NoDeTeste.NoAtual)]
+
+    elif j == 3:
+        # Greedy
+        while True:
+            break
+            NoDeTeste = RetornoMelhorCaminho(Abertos)
+            Fechados[str(NoDeTeste.NoAtual)] = NoDeTeste
+
+            if NoDeTeste.NoAtual == Objetivo:
+                print("Dfs")
+                return ConstruirCaminho(Fechados)
+
+            NosAdjacentes = NoAdjacente(NoDeTeste)
 
 
 '''
@@ -175,16 +194,18 @@ def Start(Entrada):
 '''
 if __name__ == '__main__':
     cont = 0
-    x = 1
+    x = 3
 
+    # Entrada Possível
     if x == 1:
-        #Entrada Possível
         Entrada = Start([[1, 2, 0], [4, 5, 3], [7, 8, 6]])
+
+    # Entrada Impossível
     elif x == 2:
-        #Entrada Impossível
         Entrada = Start([[8, 1, 2], [0, 4, 3], [7, 6, 5]])
+
+    # Entrada Aleatória
     elif x == 3:
-        #Entrada Aleatória
         V = sample(range(0, 9), 9)
         Entrada = Start([[V[0], V[1], V[2]], [V[3], V[4], V[5]], [V[6], V[7], V[8]]])
 
