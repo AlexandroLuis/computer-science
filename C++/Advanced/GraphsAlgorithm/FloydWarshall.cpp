29f716d94fddd06cb9c10412aeb910b53ce99b4f
Alex <52381045+AlexandroLuis@users.noreply.github.com>
2021-10-08 17:14:08 -0300

    Update FloydWarshall.cpp
    ERRO NA ULTIMA APRESENTAÇÃO, VALOR ERRADO!!!
    Lets que lets
    Deu ruim
    Lets que lets
    Deu ruim
    Deu ruim
    Deu ruim
    Deu ruim
    Lets que lets
    Deu ruim
    ----------------------------
    Lets que lets
    Deu ruim
    Lets que lets
    Deu ruim
    Deu ruim
    Deu ruim
    Deu ruim
    Lets que lets
    Lets que lets

--- C++/Advanced/GraphsAlgorithm/FloydWarshall.cpp
+++ C++/Advanced/GraphsAlgorithm/FloydWarshall.cpp
@@ -1,48 +1,46 @@
 #include <iostream>
 
+#define MAX 999
+int Distancia[MAX][MAX];
+
 using namespace std;
 
-void FloydWarshall(int distancia[999][999], int a){
+void FloydWarshall(int a){
 	for(int i = 0; i <= a; i++){
 		for(int j = 0; j <= a; j++){
 			for(int k = 0; k <= a; k++){
-				if((distancia[j][i] + distancia[i][k]) < distancia[j][k])
-                    distancia[j][k] = distancia[j][k] + distancia[i][k];
+				if((Distancia[j][i] + Distancia[i][k]) < Distancia[j][k])
+                    Distancia[j][k] = Distancia[j][k] + Distancia[i][k];
 			}
 		}
 	}
 }
 
-
 int main(){
-	int a, b, c;
+	int a, b, c, d, e;
 	
 	cin >> a >> b >> c;
 	
-	int Val[999][999], d, e;
-	
     for(int i = 0; i <= a; i++){
         for(int j = 0; j <= a; j++){
-            Val[i][j] = 999;
+            Distancia[i][j] = MAX;
         }
     }
 	
-	for(int i = 0; i <= b; i++){
+	for(int i = 0; i < b; i++){
 		cin >> d >> e;
-		Val[d][e] = 1;
-		Val[e][d] = 1;
+		Distancia[d][e] = 1;
+		Distancia[e][d] = 1;
 	}
 	
-	FloydWarshall(Val, a);
-	
+	FloydWarshall(a);
 	for(int i = 0; i < c; i++){
 		cin >> a >> b;
 		
-		if((Val[a][b] == 999)||(Val[b][a] == 999))
+		if((Distancia[a][b] == MAX)||(Distancia[b][a] == MAX))
 			cout << "Deu ruim" << endl;
         else
             cout << "Lets que lets" << endl;
 	}
-	
 	return 0;
 }
