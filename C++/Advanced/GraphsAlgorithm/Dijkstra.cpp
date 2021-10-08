a15ec25b58a91c2e1a358db17de42bba33c2b2f7
Alex <52381045+AlexandroLuis@users.noreply.github.com>
2021-10-08 16:38:17 -0300

    Update Dijkstra.cpp

--- C++/Advanced/GraphsAlgorithm/Dijkstra.cpp
+++ C++/Advanced/GraphsAlgorithm/Dijkstra.cpp
@@ -1,36 +1,41 @@
 #include <iostream>
+#include <cstdio>
+#include <stdlib.h>
+#include <limits.h>
 
 #define MAX 999
+int matriz[MAX][MAX];
+
 using namespace std;
 
-int dijkstra(int a, int b, int c, int matriz[MAX][MAX]){
-    int Distancia[a], MenorDistancia, pv = 0;
-    bool Visitados[a];
+int dijkstra(int a, int b, int c){
+    int Distancia[MAX], MenorDistancia, pv = 0;
+    bool Visitados[MAX];
 
-    for(int i = 0; i < a; i++){
-        Distancia[i] = 0;
+    for(int i = 1; i < a+1; i++){
+        Distancia[i] = MAX;
         Visitados[i] = false;
     }
 
-    Distancia[b] = -1;
+    Distancia[b] = 0;
 
-    for(int i = 0; i < a; i++){
-        MenorDistancia = 0;
+    for(int i = 1; i < a+1; i++){
+        MenorDistancia = MAX;
 
         if(Visitados[c]) break;
 
-        for(int j = 0; j < a; j++){
+        for(int j = 1; j < a+1; j++){
             if(!Visitados[j] && (Distancia[j] < MenorDistancia)){
                 MenorDistancia = Distancia[j];
                 pv = j;
             }
         }
 
-        if(MenorDistancia == 0) break;
+        if(MenorDistancia == MAX) break;
 
         Visitados[pv] = true;
 
-        for(int j = 0; j < a; j++){
+        for(int j = 1; j < a+1; j++){
             int Soma = MenorDistancia + matriz[pv][j];
             if(Soma < Distancia[j])
                 Distancia[j] = Soma;
@@ -41,33 +46,29 @@ int dijkstra(int a, int b, int c, int matriz[MAX][MAX]){
 
 int main(){
     int a, b, c, d, e, f, g, h;
-    int Val[MAX][MAX];
 
     while(scanf("%d%d", &a, &b) && (a||b)){
-        for(int i = 0; i < a; i++){
-            for(int j = 0; j < a; j++)
-                Val[i][j] = 0;
-         }
-         for(int i = 0; i < a; i++){
+        for(int i = 1; i <= a; i++){
+            for(int j = 1; j <= a; j++)
+                matriz[i][j] = MAX;
+        }
+        for(int i = 1; i <= b; i++){
             scanf("%d%d%d", &c, &d, &e);
-            Val[c][d] = e;
-            if(Val[d][c] != 0){
-                Val[d][c] = 0;
-                Val[c][d] = 0;
-            }
-         }
+            matriz[c][d] = e;
+            if(matriz[d][c] != MAX)
+                matriz[d][c] = matriz[c][d] = 0;
+        }
 
-         scanf("%d", &f);
-
-         for(int i = 0; i < f; i++){
+        scanf("%d", &f);
+        for(int i = 0; i < f; i++){
             scanf("%d%d", &g, &h);
-            int Result = dijkstra(a, g, h, Val);
-            if(Result == 0)
+            int Result = dijkstra(a, g, h);
+            if(Result == MAX)
                 printf("Nao e possivel entregar a carta\n");
             else
                 printf("%d\n", Result);
-         }
-         printf("\n");
+        }
+        printf("\n");
     }
     return 0;
 }
