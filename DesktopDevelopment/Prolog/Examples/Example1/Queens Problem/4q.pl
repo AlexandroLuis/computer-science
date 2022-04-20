4dba1ca899df36bce9dc94c2813b2eaa07c74477
Alex <52381045+AlexandroLuis@users.noreply.github.com>
2022-04-20 14:32:10 -0300

    Create 4q.pl

---
+++ DesktopDevelopment/Prolog/Examples/Example1/Queens Problem/4q.pl
@@ -0,0 +1,27 @@
+rainha(N, rainha) :-
+    length(rainha, N),
+	tabuleiro(rainha, tabuleiro, 0, N, _, _),
+	rainha(tabuleiro, 0, rainha).
+
+tabuleiro([], [], N, N, _, _).
+tabuleiro([_|rainha], [Col-Vars|tabuleiro], Col0, N, [_|VR], VC) :-
+	Col is Col0+1,
+	functor(Vars, f, N),
+	constraints(N, Vars, VR, VC),
+	tabuleiro(rainha, tabuleiro, Col, N, VR, [_|VC]).
+
+constraints(0, _, _, _) :- !.
+constraints(N, Row, [R|Rs], [C|Cs]) :-
+	arg(N, Row, R-C),
+	M is N-1,
+	constraints(M, Row, Rs, Cs).
+
+rainha([], _, []).
+rainha([C|Cs], Row0, [Col|solucao]) :-
+	Row is Row0+1,
+	select(Col-Vars, [C|Cs], tabuleiro),
+	arg(Row, Vars, Row-Row),
+	rainha(tabuleiro, Row, solucao).
+
+
+/** Para Chamar a Função Use: "rainha(4, rainha)." **/
